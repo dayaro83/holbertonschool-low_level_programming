822e9272aa0e586502043a347eab9a1be2ee9866
dayaro83 <yamiler0925@gmail.com>
2022-10-19 00:17:10 -0700

    1-swap.c

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,18 +1,15 @@
-/**
- * swap_int - swaps the values of two integers
- *using two input parameters
- *
- *@a: input parameter 1
- *@b: imput parameter 2
- *
- *Return: Nothing
- */
+#include "main.h"
 
+/**
+* swap_int - swaps the values of two integers swap the values
+*@a: input parameter 1
+*@b: imput parameter 2
+*
+*/
 void swap_int(int *a, int *b)
 {
-  int temp;
-
-  temp = *a;
-  *a = *b;
-  *b = temp;
+int tmp;
+tmp = *a;
+*a = *b;
+*b = tmp;
 }
