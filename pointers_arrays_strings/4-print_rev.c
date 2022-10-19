f8a3be146813a78f58ab48dbcbc6de0176ba42b7
dayaro83 <yamiler0925@gmail.com>
2022-10-19 02:17:50 -0700

    4-print

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* print_rev -> printing a string in reverse
+*@: the string to be printed in rev
+*/
+void print_rev (char *s)
+{
+int i, n;
+s = "hello"
+n = strlens (s);
+_put char(s[i]);
+}
