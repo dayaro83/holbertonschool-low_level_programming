a7f1fd2df66c2ab31d61ce392beac21fb765b9d9
dayaro83 <yamiler0925@gmail.com>
2022-10-23 09:42:12 -0700

    task5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main -Entry point
+*
+*Retur: Always 0 (Success)
+*/
+int main (void)
+{
+  printf ("whith proper gramar, but the outcome is a piece of art,\n");
+  retur (0)
+}
