1b61fa5de427ac7b6bd328b28db39ec20c358f40
dayaro83 <yamiler0925@gmail.com>
2022-10-25 13:20:17 -0700

    taskprint_alphabet1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,16 +1,15 @@
+#include "main.h"
 /**
- * print_alphabet - entry point
- *
- * Description: Prints the alphabet with _putchar
- *
- * Return: void
- */
+*print_alphabet - a function that prints the alphabet, in lowercase
+*
+*Return: Always 0 (Succes)
+*/
 void print_alphabet(void)
 {
-char c
-for (c = 'a'; c <= 'z'; c++)
+int n;
+for (n = 'a'; n <= 'z'; n++)
 {
-_putchar(c);
+_putchar(n);
 }
 _putchar('\n');
 }
